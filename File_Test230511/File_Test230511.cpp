79b9db74c8538159f03f0ffb29f2ae7cfc3cfbd2
xioalong <1982518071@qq.com>
2023-07-29 22:56:38 +0800

    aaaaa

---
+++ File_Test230511/File_Test230511.cpp
@@ -0,0 +1,89 @@
+﻿// File_Test230511.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ShlObj.h>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+//unicode转ansi
+std::string UnicodeToANSI(const std::wstring& str)
+{
+	char* pElementText;
+	int    iTextLen;
+	// wide char to multi char
+	iTextLen = ::WideCharToMultiByte(CP_ACP, 0, str.c_str(), -1, NULL, 0, NULL, NULL);
+	pElementText = new char[iTextLen + 1];
+	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
+	::WideCharToMultiByte(CP_ACP, 0, str.c_str(), -1, pElementText, iTextLen, NULL, NULL);
+	std::string strText;
+	strText = pElementText;
+	delete[] pElementText;
+	return strText;
+}
+
+
+//选择文件夹代码
+bool func(string& sFolder)
+{
+	BROWSEINFO  bi;
+	bi.hwndOwner = NULL;
+	bi.pidlRoot = CSIDL_DESKTOP;//文件夹的根目录，此处为桌面
+	bi.pszDisplayName = NULL;
+	bi.lpszTitle = NULL;//显示位于对话框左上部的提示信息
+	bi.ulFlags = BIF_DONTGOBELOWDOMAIN | BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;//有新建文件夹按钮
+	bi.lpfn = NULL;
+	bi.iImage = 0;
+	LPITEMIDLIST pidl = SHBrowseForFolder(&bi);//调用选择对话框
+	if (pidl == NULL)
+	{
+		//如果关闭对话框
+		return false;
+	}
+	TCHAR strFolder[MAX_PATH];
+	SHGetPathFromIDList(pidl, strFolder);
+	sFolder = UnicodeToANSI(strFolder);
+	return false;
+}
+
+
+void fileRead(string& path)
+{
+    string temp;
+    ifstream filePath(path);
+	//cout << path << endl;
+    if (!filePath.is_open())
+    {
+        cout << "打开失败" << endl;
+        return;
+    }
+    while (getline(filePath,temp))
+    {
+        cout << temp << endl;
+    }
+    filePath.close();
+}
+
+int main()
+{
+	string filePath;
+	func(filePath);
+	fileRead(filePath);
+    //fileRead();
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
